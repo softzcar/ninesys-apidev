c0037f5f2dc71068b8fa2467c08b0bc91ff8e4b3
Ozcar <softzcar@gmail.com>
2026-03-30 11:57:21 -0400

    Enable errors in check_indexes.php

--- public/check_indexes.php
+++ public/check_indexes.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../app/config.php';
 require __DIR__ . '/../app/app_loader.php';
