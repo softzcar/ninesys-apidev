71d38a78834f7c475e889be2d902efcea0144163
Ozcar <softzcar@gmail.com>
2026-04-21 11:46:55 -0400

    Ajuste en giardar id_insumo al porcesar el material por aprte del empelado

--- test_db.php
+++ test_db.php
@@ -1,34 +1,8 @@
 <?php
-require 'vendor/autoload.php';
-$app = new \Slim\App(['settings' => ['displayErrorDetails' => true]]);
-require 'app/config/db.php';
-$localConnection = new LocalDB();
-$sql = "SELECT _id, form, tipo, id_empleado, empleado, observaciones, productos_json FROM (
-              SELECT a._id, 
-                     a.form, 
-                     a.tipo, 
-                     b.id_usuario AS id_empleado, 
-                     b.nombre AS empleado,
-                     JSON_UNQUOTE(JSON_EXTRACT(a.form, '$.obs')) as observaciones,
-                     JSON_EXTRACT(a.form, '$.productos') as productos_json
-              FROM ordenes_tmp a 
-              JOIN api_empresas.empresas_usuarios b ON a.id_empleado = b.id_usuario
-              UNION ALL
-              SELECT p._id, 
-                     CONCAT('{\"id_presupuesto_original\":', p._id, ',\"nombre\":\"', p.cliente_nombre, '\",\"cedula\":\"', p.cliente_cedula, '\",\"total\":', p.pago_total, ',\"presupuesto_emitido\":true}') as form, 
-                     'Presupuesto Finalizado' as tipo, 
-                     p.responsable as id_empleado, 
-                     u.nombre as empleado,
-                     p.observaciones,
-                     (SELECT JSON_ARRAYAGG(JSON_OBJECT('name', pp.name, 'cantidad', pp.cantidad, 'talla', s.nombre, 'tela', t.tela)) 
-                      FROM presupuestos_productos pp 
-                      LEFT JOIN sizes s ON pp.id_size = s._id 
-                      LEFT JOIN catalogo_telas t ON pp.id_tela = t._id
-                      WHERE pp.id_orden = p._id) as productos_json
-              FROM presupuestos p
-              JOIN api_empresas.empresas_usuarios u ON p.responsable = u.id_usuario
-              WHERE p.status != 'Convertido'
-            ) as combined
-            ORDER BY _id DESC LIMIT 100";
-$results = $localConnection->goQuery($sql);
-var_dump($results === false ? "Connection or Query Error: " . $localConnection->error : count($results));
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/includes/config.php';
+require_once __DIR__ . '/includes/db.php';
+
+$db = new LocalDB();
+$res = $db->goQuery("SELECT * FROM inventario_movimientos WHERE id_orden = 4519");
+print_r($res);
