47be684310378f669258e1968ba59a8ee83495ed
Ozcar <softzcar@gmail.com>
2026-05-26 17:38:31 -0400

    debug(replenishment): update query helper to fetch last 10 repos

--- public/check_repo.php
+++ public/check_repo.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../src/LocalDB.php';
 header('Content-Type: application/json');
 
 $db = new LocalDB();
-$sql = "SELECT _id, id_orden, detalle, detalle_emisor, aprobada, moment FROM reposiciones WHERE id_orden = 5176 ORDER BY _id DESC LIMIT 5";
+$sql = "SELECT _id, id_orden, detalle, detalle_emisor, aprobada, moment FROM reposiciones ORDER BY _id DESC LIMIT 10";
 $res = $db->goQuery($sql);
 echo json_encode($res, JSON_PRETTY_PRINT);
 $db->disconnect();
