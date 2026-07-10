debf9961a2d9b82b1a192669f2da0140ab024658
Ozcar <softzcar@gmail.com>
2026-07-10 10:34:19 -0400

    fix(pgsql): escapar comillas simples de los literales en consulta de disenadores

--- app/routes/payments.php
+++ app/routes/payments.php
@@ -531,7 +531,7 @@ return function (App $app) {
                   pagos p
               JOIN revisiones r ON
                   p.id_orden = r.id_orden AND p.id_empleado = r.id_empleado AND r.estatus = "Aprobado"
-              WHERE p.fecha_pago IS NULL AND p.detalle IN ("Diseño", "ajuste", "personalización")' . $whereFecha . '
+              WHERE p.fecha_pago IS NULL AND p.detalle IN (\'Diseño\', \'ajuste\', \'personalización\')' . $whereFecha . '
               GROUP BY p._id
           ';
     }
